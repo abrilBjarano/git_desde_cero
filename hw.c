da50d00e0cb84d0ae865642f031696c3614b61c2
Abril Bejarano <abrlbjrn@gmail.com>
2020-12-21 21:04:53 -0600

    Agregar operacion

--- hw.c
+++ hw.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 void main(void){
     printf("\nHola mundo\n");
+
+    int r;
+
+    print("\nDigite un numero: ");
+    scanf("%d",&r);
+
+    int a;
+
+    a = r * 2;
+
+    printf("\n%d\n",a);
 }
